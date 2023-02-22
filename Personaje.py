0d98934cabefc042f7eba58640b91ff7dc14b72d
JessicaBarradas <121040932@upq.edu.mx>
2023-02-22 16:30:41 -0600

    Definimos el constructor  del personaje

--- Personaje.py
+++ Personaje.py
@@ -1,16 +1,18 @@
 class Personaje:
-    #atributos del personaje por default
-    especie= "humano"
-    nombre= "Master Chief"
-    altura= "2.70"
+
+    #Definimos el constructor de personaje
+    def _init_(self,esp,nom,alt):
+     self.especie= esp
+     self.nombre= nom
+     self.altura= alt
     #metodos del personaje
     def correr(self,status):
         if(status):
-            print("el personaje " + self.nombre + "esta corriendo")
+            print("el personaje " + self.nombre + " esta corriendo")
         else:
-            print("el personaje " + self.nombre + "se detuvo")
+            print("el personaje " + self.nombre + " se detuvo")
     def lanzarGranadas(self):
-            print("el personaje " + self.nombre + "lanzo una granada")
+            print("el personaje " + self.nombre + " lanzo una granada")
     def recargarArma(self, municiones):
         cargador= 10
         cargador= cargador + municiones
--- objetos.py
+++ objetos.py
@@ -1,4 +1,21 @@
 from Personaje import *
+
+#1.Solicitar datos
+print("")
+print("####### Datos Heroe #")
+especieH=input("Escribe la especie del Heroe: ")
+nombreH=input("Escribe el nombre del Heroe: ")
+alturaH=input("Escribe la altura del Heroe: ")
+recargarH=input("Cuantas balas recarga el Heroe: ")
+
+print("")
+print("####### Datos Villano #")
+especieV=input("Escribe la especie del villano: ")
+nombreV=input("Escribe el nombre del villano: ")
+alturaV=input("Escribe la altura del villano: ")
+recargarV=input("Cuantas balas recarga el villano: ")
+
+
 #1. Crear objeto de clase personaje
 heroe= Personaje()
 #2. Usar atributos
