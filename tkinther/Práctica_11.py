08623e373f12bbe0dd73f90ed588a77824660f4f
JessicaBarradas <121040932@upq.edu.mx>
2023-03-01 16:21:05 -0600

    funciones  con botones

--- tkinther/Práctica_11.py
+++ tkinther/Práctica_11.py
@@ -1,6 +1,16 @@
 from tkinter import Tk,Button,Frame,messagebox
 def mostrarMensajes():
     messagebox.showinfo("showinfo", "Presionaste el boton Azul")
+    messagebox.showerror("ERROR","Todo fallo con exito")
+    print(messagebox.askokcancel("Pregunta","¿Ella o el jugó con tu corazon?"))
+    messagebox.askquestion("PRUEBA","puedes, o no puedes?")
+    messagebox.askretrycancel("PRUEBA","No creo mi ciela")
+    messagebox.askyesno("Prueba","quieres ser mi chava?")
+#6 Funcion para agregar botones
+def agregarBoton():
+    botonVerde.config(text="+",bg="green",fg="white")
+    botonNuevo=Button(seccion3,text="Nuevo")
+    botonNuevo.pack()
 #1.Instanciamos el objeto ventana
 ventana=Tk()
 ventana.title("Ejemplo de 3 Frames ")
@@ -19,7 +29,7 @@ botonAmarillo=Button(seccion2,text="Boton Amarillo",bg="#FFF933")
 botonAmarillo.grid(row=1, column=1)
 botonNegro=Button(seccion2,text="Boton Negro",fg="white",bg="#000000") 
 botonNegro.grid(row=0, column=0)
-botonVerde=Button(seccion3,text="Boton Verde", bg="#46C91F")
+botonVerde=Button(seccion3,text="Boton Verde", bg="#46C91F",command=agregarBoton)
 botonVerde.configure(height=2,width=10)
 botonVerde.pack()
 #llamamos al Main
