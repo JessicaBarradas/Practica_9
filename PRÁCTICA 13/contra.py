039f39dcaac9b991426d34f4e63f048e60da5769
JessicaBarradas <121040932@upq.edu.mx>
2023-03-08 17:20:29 -0600

    agregue los def

---
+++ PRÁCTICA 13/contra.py
@@ -0,0 +1,11 @@
+class contra():
+    def __init__(self,letras,digitos,puntuacion):
+        self.__letters=letras
+        self.__digitos=digitos
+        self.__puntuacion=puntuacion
+    def letras(self):
+        return self.__letters
+    def digitos(self):
+        return self.__digitos
+    def puntuacion(self):
+        return self.__puntuacion
